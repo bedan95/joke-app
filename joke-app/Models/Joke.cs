237a91c19d88adbb8981bb5671e2d7ddb9eb93ce
Benda Dániel <benda.daniel@infopolus.hu>
2022-09-21 07:25:03 +0200

    30:00

---
+++ joke-app/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace joke_app.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
